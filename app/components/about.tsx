510087a6467a07c261f3be81598333ea98851fb5
knlrvr <kanelariviere@outlook.com>
2025-04-20 21:23:34 -0400

    work update

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -7,9 +7,8 @@ export default function About() {
                 <span className="font-medium">Hello!</span>
                 <p className="text-neutral-600 dark:text-neutral-300 blog">
                     I&apos;m a <span className="underline underline-offset-2 decoration-neutral-300">software engineer</span>,
-                    artist, and d&d nerd living in Charlotte, NC.
-                    I&apos;m currently working at <Link href="/work/fenili" className="underline underline-offset-2 decoration-neutral-300 hover:decoration-neutral-500 transition-colors duration-150">Fenili Apparel</Link> as a software engineer.
-                    Crafting engaging and memorable experiences while exploring sustainability and advocating 
+                    artist, and d&d nerd living in North Carolina.
+                    Focused on crafting engaging and memorable experiences while exploring sustainability and advocating 
                     for a greener web. <Link href="https://github.com/knlrvr" className="underline underline-offset-2 decoration-neutral-300 hover:decoration-neutral-500 transition-colors duration-150">Always shipping</Link>.
                 </p>
             </div>
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -14,56 +14,33 @@ export default function Work() {
         <div className="max-w-2xl mx-auto">
             <PageTop 
                 title="Kane Lariviere"
-                desc="Experience & Selected Work"
+                desc="Work"
             />
 
             <div className="flex flex-col space-y-12 mt-8 text-sm">
                 
                 <WorkCard 
-                    org="Fenili Apparel"
-                    role="Software Engineer"
-                    start="2024"
+                    org="eraWorks"
+                    role="Founder + Software Engineer"
+                    start="2025"
                     end="Present"
-                    desc="Lead the design & development of e-commerce storefront & admin tools. Collaborating closely with CEO & Product Team to track objectives & ship frequently."
+                    desc="Leading the design & development of web projects &mdash; helping businesses express themselves online."
                 />
 
                 <WorkCard
                     org="Freelance"
                     role="Software Engineer"
                     start="2022"
-                    end="2024"
+                    end="Present"
                     desc="Worked with a range of small to medium businesses, agencies, start-ups, and individuals to build products, experiences, and improve technical direction."
                 />
 
-                <hr className="border-neutral-400" />
-
-                <div className="space-y-16">
-                    <ProjectCard
-                        img="/fenili-brand.png"
-                        title="Fenili Apparel"
-                        desc="An ecommerce storefront for the Fenili apparel & lifestyle brand."
-                        date="2024 ~"
-                        url="/fenili"
-                    />
-                    <ProjectCard
-                        img="/studico-brand.png"
-                        title="Studico"
-                        desc="A web-based music production assistance suite that empowers teams of all sizes to create, collaborate, and share their music."
-                        date="2024 ~"
-                        url="/studico"
-                    />
-                    <ProjectCard
-                        img="/tsif-brand.png"
-                        title="The Sky Is Fake"
-                        desc="An image sharing platform dedicated to pictures of the sky that just don't seem real."
-                        date="2024"
-                        url="/theskyisfake"
-                    />
-                </div>
             </div>
-            <Link href="https://github.com/knlrvr" target="_blank" className="flex items-center gap-1 text-sm text-neutral-500 group w-fit mt-16 mb-8">
-                <span className="text-xs text-neutral-500 underline underline-offset-2 decoration-neutral-300 hover:decoration-neutral-500 dark:decoration-neutral-500 dark:hover:decoration-neutral-300 transition duration-100">View More on GitHub</span>
+
+            <Link href="https://github.com/knlrvr" target="_blank" className="flex items-center gap-1 text-sm text-neutral-500 group w-fit my-16">
+                <span className="text-xs text-neutral-500 underline underline-offset-2 decoration-neutral-300 hover:decoration-neutral-500 dark:decoration-neutral-500 dark:hover:decoration-neutral-300 transition duration-100">View My GitHub</span>
             </Link>
+
         </div>
     )
 }
