e8dd94e49936becc15d44c32f3987971ad1a5075
knlrvr <kanelariviere@outlook.com>
2025-04-25 10:57:25 -0400

    ffs

--- app/components/effects/grain.tsx
+++ app/components/effects/grain.tsx
@@ -50,7 +50,7 @@ export const Grain = () => {
     <div className="pointer-events-none fixed inset-0 h-full w-full overflow-hidden z-[9999]">
       <div
         ref={grainRef}
-        className="absolute inset-[-200%] h-[400%] w-[400%] bg-[url('/framernoise.png')] bg-[length:256px] bg-left-top opacity-[3%]"
+        className="absolute inset-[-200%] h-[400%] w-[400%] bg-[url('/framernoise.png')] bg-[length:256px] bg-left-top opacity-[3%] dark:opacity-[2%]"
       />
     </div>
   );
--- app/components/post-preview.tsx
+++ app/components/post-preview.tsx
@@ -10,7 +10,7 @@ const PostPreview = (props: PostMetadata) => {
         <p className="font-medium tracking-tight">{props.title}</p>
         <p className="text-neutral-600 dark:text-neutral-400">{props.description}</p>
         
-        <span className="text-neutral-500 text-xs underline underline-offset-2 decoration-neutral-300 group-hover:decoration-neutral-500 dark:decoration-neutral-500 dark:group-hover:decoration-neutral-300 group-hover:decoration-neutral-500 transition-colors duration-150 mt-4">Read More</span>
+        <span className="text-neutral-500 text-xs underline underline-offset-2 decoration-neutral-300 group-hover:decoration-neutral-500 dark:decoration-neutral-500 dark:group-hover:decoration-neutral-300 transition-colors duration-150 mt-4">Read More</span>
       </Link>
     )
 }
