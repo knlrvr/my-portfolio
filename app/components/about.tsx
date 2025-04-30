7af245ce064d1eefa1d9f47000b8862457c9c250
knlrvr <kanelariviere@outlook.com>
2025-04-29 21:37:13 -0400

    rm link

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -12,10 +12,6 @@ export default function About() {
                     for a greener web. <Link href="https://github.com/knlrvr" className="underline underline-offset-2 decoration-neutral-300 hover:decoration-neutral-500 transition-colors duration-150">Always shipping</Link>.
                 </p>
             </div>
-
-            <Link href="/work" className="flex items-center gap-1 text-sm w-fit my-4">
-                <span className="text-neutral-500 text-xs underline underline-offset-2 decoration-neutral-300 hover:decoration-neutral-500 dark:decoration-neutral-500 dark:hover:decoration-neutral-300 transition-colors duration-150">View My Work</span>
-            </Link>
         </div>
     )
 }
