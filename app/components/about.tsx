b57fcd017191933df2606a05e1ced5836c3ef2fd
knlrvr <kanelariviere@outlook.com>
2025-05-09 19:19:51 -0400

    edit about

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -7,7 +7,7 @@ export default function About() {
                 <span className="font-medium">Hello!</span>
                 <p className="text-neutral-600 dark:text-neutral-300 blog">
                     I&apos;m a <span className="underline underline-offset-2 decoration-neutral-300">software engineer</span>,
-                    artist, and d&d nerd living in North Carolina. Currently working at <span className="underline underline-offset-2 decoration-neutral-300">eraWorks Studio</span>.
+                    artist, and d&d nerd living in North Carolina.
                     Focused on crafting engaging and memorable experiences while exploring sustainability and advocating 
                     for a greener web. <Link href="https://github.com/knlrvr" className="underline underline-offset-2 decoration-neutral-300 hover:decoration-neutral-500 transition-colors duration-150">Always shipping</Link>.
                 </p>
