d2adf56c08c0035cea8f96ccdfecd4cc2402d25f
knlrvr <kanelariviere@outlook.com>
2025-07-09 11:46:51 -0400

    styling: not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
             />
 
         <div className="my-8">
-            <p className="text-sm text-neutral-600">
+            <p className="text-sm text-neutral-600 dark:text-neutral-400">
                 <span className="underline underline-offset-2 decoration-neutral-300">Sorry about that</span>! There doesn&apos;t seem to be anything here. You can check the URL and try again, click one of the links below,
                 or return to the <Link href="/" className="underline underline-offset-2 decoration-neutral-300 hover:decoration-neutral-500 transition-colors duration-150">home page</Link>.
             </p>
