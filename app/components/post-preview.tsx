843f0d1cc092831ce79e2ca9d03f79d6ba6e820a
knlrvr <kanelariviere@outlook.com>
2026-03-13 22:09:22 -0400

    PIXEL PERFECT

--- app/components/post-preview.tsx
+++ app/components/post-preview.tsx
@@ -13,9 +13,9 @@ const PostPreview = (props: PostMetadata) => {
             <p className="font-medium tracking-tighter group-hover:text-mist-400 transition-colors duration-150">{props.title}</p>
             <LuArrowRight className='group-hover:text-mist-500 group-hover:translate-x-1 transition-all duration-150'/>
           </div>
-          <div className="flex items-center justify-between text-sm">
+          <div className="flex items-center justify-between text-sm gap-4">
             <p className="text-neutral-700 dark:text-neutral-300">{props.description}</p>
-            <p className="text-mist-500 font-mono text-[11px] text-right">{props.prevDate}</p>
+            <p className="text-mist-500 font-mono text-[11px] text-right mt-0.5">{props.prevDate}</p>
           </div>
           <p className="text-mist-400 dark:text-mist-600 text-[11px] font-mono">{props.readTime}</p>
       </Link>
